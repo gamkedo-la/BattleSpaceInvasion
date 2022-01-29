a9dc300a0e213c7d2f4ab413837012d12c3f6f95
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-01-29 11:28:29 +0800

    Initial commit

---
+++ Assets/Scripts/Galaxy_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Galaxy_Script : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 scrollSpeed;
+
+    private Material materialToScroll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialToScroll = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        materialToScroll.mainTextureOffset += scrollSpeed * Time.deltaTime;
+    }
+}
