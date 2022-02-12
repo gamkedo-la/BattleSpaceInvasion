0dd2d53fc16b67df3770b4cfa292c86c5c73d948
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-12 22:23:15 +0800

    add Asteroid script and apply rotation

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{   
+    public float degreesPerSec = 360f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotAmount = degreesPerSec * Time.deltaTime;
+        float currentRotation = transform.localRotation.eulerAngles.z;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRotation + rotAmount));
+    }
+}
