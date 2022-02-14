317a558f5b27374aee73d755f73f5f6a4793e6d1
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-14 20:13:57 +0800

    applied localScale to resize redSpaceShip on update and movement to left - WIP

--- Assets/Scripts/Red_SpaceShip_Enemy1.cs
+++ Assets/Scripts/Red_SpaceShip_Enemy1.cs
@@ -7,16 +7,19 @@ public class Red_SpaceShip_Enemy1 : MonoBehaviour
 
     private float speed = 5.0f;
 
+    private Vector3 scaleChange;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(9, 4, 0);
+        transform.position = new Vector3(13, 4, 0);
+        scaleChange = new Vector3(0.6f, 0.6f, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.localScale = scaleChange; // used localScale property to scale object. 
         redSpaceshipMovement();
     }
 
