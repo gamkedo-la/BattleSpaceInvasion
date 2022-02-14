e46e02a3d223e526a9b6ab9cc6fff608cd7bbc95
cookieoverflow <hey@cookieoverflow.com>
2022-02-14 23:11:08 +0000

    Add explosion particle effect for Asteroid and laser collision detection on the asteroid script.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
-{   
+{
+    public GameObject explosion;
     public float degreesPerSec = 360f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,14 @@ public class Asteroid : MonoBehaviour
         float rotAmount = degreesPerSec * Time.deltaTime;
         float currentRotation = transform.localRotation.eulerAngles.z;
         transform.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRotation + rotAmount));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "laser")
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
