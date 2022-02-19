e886ed4782e5bcc3cc0509cbc65c41114c83cc3f
TheHaad <fmuntaz@gmail.com>
2022-02-19 11:16:12 -0500

    add power_up scriptable object script

---
+++ Assets/Scripts/Power_Up.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="ScriptableObjects/PowerUp", order = 1)]
+public class Power_Up : ScriptableObject
+{
+    enum PowerUpCategory
+    {
+        Speed,
+        Weapon
+    };
+
+    [SerializeField] PowerUpCategory category;
+    [SerializeField] int multiplier = 0;
+    [SerializeField] int damage = 0;
+    [SerializeField] string prefab_name;
+
+}
