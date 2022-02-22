6e91794385729d0db93f2e4e7454f6c13aa868b0
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-02-22 22:48:32 +0800

    reduce treemonster1 size and add movement down after spawning - WIP

--- Assets/Scripts/TreeMonster1.cs
+++ Assets/Scripts/TreeMonster1.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TreeMonster1 : MonoBehaviour
 {
+    private float speed = 5.0f;
+    private Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, 4, 0);
+        scaleChange = new Vector3(0.2f, 0.2f, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = scaleChange; // used localScale property to scale object. 
+        treeMonster1_Movement();
+    }
+
+
+    void treeMonster1_Movement()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/TreeMonsterBig_SpaceTree.cs
+++ Assets/Scripts/TreeMonsterBig_SpaceTree.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TreeMonsterBig_SpaceTree : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
