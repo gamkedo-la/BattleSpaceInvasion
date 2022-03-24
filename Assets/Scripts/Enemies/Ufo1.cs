418e19f836d855bdb7a198be1a48c2ef7c8fbc0e
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-03-24 18:24:23 +0800

    assigned to ufo1 script player jetspaceship

--- Assets/Scripts/Enemies/Ufo1.cs
+++ Assets/Scripts/Enemies/Ufo1.cs
@@ -8,7 +8,9 @@ public class Ufo1 : MonoBehaviour
     //CONSTANT VARIABLES
     private float speed = 4.0f;
 
-    //
+    //calculation of direction of player
+    public Transform player;
+
 
 
     // Start is called before the first frame update
@@ -20,12 +22,16 @@ public class Ufo1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 direction = player.position - transform.position;
+        Debug.Log(direction); //test distance between ufo1
         Ufo1Movement();
         if (transform.position.x < 4.9f)
         {
           // float random = Random.Range(-2.5f, 7f);
 
             transform.position = new Vector3(0, -2.5f, 0);
+
         }
 
         
