87f2fe10f468f72e9950cce355ae99f089243f7e
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-03-27 18:37:10 +0800

    Test adding an offset to the treeWorld

--- Assets/Scripts/Levels/Jungle_Script.cs
+++ Assets/Scripts/Levels/Jungle_Script.cs
@@ -9,7 +9,7 @@ public class Jungle_Script : MonoBehaviour
     [SerializeField]
     private Vector2 scrollSpeed;
 
-    private float timer = 6.1f;
+    //private float timer = 6.1f;
 
     private Material materialToScroll;
     // Start is called before the first frame update
@@ -23,12 +23,12 @@ public class Jungle_Script : MonoBehaviour
     {
         materialToScroll.mainTextureOffset += scrollSpeed * Time.deltaTime;
 
-        timer -= Time.deltaTime;
+       // timer -= Time.deltaTime;
         
-        if (timer <= 0 ) // set timer and collider for entrance to ruins scene object.
-        {
-            SceneManager.LoadScene("CaveLevel");
-        }
+        //if (timer <= 0 ) // set timer and collider for entrance to ruins scene object.
+        //{
+        //    SceneManager.LoadScene("CaveLevel");
+        //}
         
     }
 }
