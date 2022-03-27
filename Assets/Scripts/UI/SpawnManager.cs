f913369ff79e90fc452ddacee661eae8b2412747
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-03-27 23:50:03 +0800

    Update spawnManager redSpaceShip Player dies

--- Assets/Scripts/UI/SpawnManager.cs
+++ Assets/Scripts/UI/SpawnManager.cs
@@ -23,12 +23,15 @@ public class SpawnManager : MonoBehaviour
 
    IEnumerator SpawnRoutine()
     {
-        Vector3 postToSpawn = new Vector3(Random.Range(7f, -7f), 4, 0);
-        Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.identity);
-        Vector3 rotationVector = new Vector3(0, 0, 90);
-        Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.Euler(rotationVector));
-
-        yield return new WaitForSeconds(5.0f);
+        while (true)
+        {
+            Vector3 postToSpawn = new Vector3(Random.Range(7f, -7f), 4, 0);
+            Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.identity);
+            Vector3 rotationVector = new Vector3(0, 0, 90);
+            Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.Euler(rotationVector));
+            yield return new WaitForSeconds(5.0f);
+        }
+        
     }
 
     //spawn game objects 
