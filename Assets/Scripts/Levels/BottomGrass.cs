60f4519339864d07810b7ed2c81e0dfec086dc2d
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-03-27 20:15:41 +0800

    add foreground mountain

--- Assets/Scripts/Levels/BottomGrass.cs
+++ Assets/Scripts/Levels/BottomGrass.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BottomGrass : MonoBehaviour
 {
+    [SerializeField]
+    private Vector2 scrollSpeed;
+
+    //private float timer = 6.1f;
+
+    private Material materialToScroll;
     // Start is called before the first frame update
     void Start()
     {
-        
+        materialToScroll = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        materialToScroll.mainTextureOffset += scrollSpeed * Time.deltaTime;
+
+        // timer -= Time.deltaTime;
+
+        //if (timer <= 0 ) // set timer and collider for entrance to ruins scene object.
+        //{
+        //    SceneManager.LoadScene("CaveLevel");
+        //}
+
     }
 }
