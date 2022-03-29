ba0fe0b50b307967e209242c67927a81a0e19901
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-03-30 06:34:08 +0800

    test clone asteroid1

--- Assets/Scripts/UI/SpawnManager.cs
+++ Assets/Scripts/UI/SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject redSpaceShipPrefab;
     [SerializeField]
+    private GameObject asteroid1Prefab;
+    [SerializeField]
     private GameObject redEnemyContainer;
 
 
@@ -30,9 +32,17 @@ public class SpawnManager : MonoBehaviour
         {
             Vector3 postToSpawn = new Vector3(Random.Range(7f, -7f), 4, 0);
             Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.identity);
+
+            Vector3 asteroid1ToSpawn = new Vector3(Random.Range(7f, -7f), 4, 0);
+            Instantiate(asteroid1Prefab, asteroid1ToSpawn, Quaternion.identity);
+
             Vector3 rotationVector = new Vector3(0, 0, 90);
+            
             GameObject newRedEnemy = Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.Euler(rotationVector));
             newRedEnemy.transform.parent = redEnemyContainer.transform;
+            
+            GameObject newAsteriod1 = Instantiate(asteroid1Prefab, asteroid1ToSpawn, Quaternion.identity);
+            newAsteriod1.transform.parent = redEnemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
         
