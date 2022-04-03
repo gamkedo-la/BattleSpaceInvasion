6a338f3bbda206919e4b1081c635de2ea8382869
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-04-03 17:27:59 +0800

    set some waypoints for mothership movements - WIP

--- Assets/Scripts/Levels/Inner_MotherShip_Boss.cs
+++ Assets/Scripts/Levels/Inner_MotherShip_Boss.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Inner_MotherShip_Boss : MonoBehaviour
 {
+
+    public GameObject motherShip;
+
+    [SerializeField]
+    Transform[] Positions;
+
+    private int NextPositionIndex;
+    Transform NextPosition;
     //public Transform targetPosition;
     [SerializeField]
     private float speed;
     private bool stopMovement = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NextPosition = Positions[0];
     }
 
     // Update is called once per frame
@@ -19,10 +28,7 @@ public class Inner_MotherShip_Boss : MonoBehaviour
     {
         //position to setting position to target.
         //transform.position = targetPosition.position;
-
         //Lerp function that provides smooth transition to target.
-
-
         //Move toward 
         //Vector3 a = transform.position;
         //Vector3 b = targetPosition.position;
@@ -34,15 +40,29 @@ public class Inner_MotherShip_Boss : MonoBehaviour
     void MoveMothership()
     {
      
-        transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
-
+        //transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
         //transform = transform.position.x;
-
-
-        //if(transform == -15 && stopMovement == true)
+        //if(transform.position.x <= -15f && stopMovement == true)
         //{
+        //    Debug.Log("X position is at -15");
         //    stopMovement = true;
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         //}
+
+        if(transform.position == NextPosition.position)
+        {
+            NextPositionIndex++;
+            if (NextPositionIndex >= Positions.Length)
+            {
+                NextPositionIndex = 0;
+            }
+            NextPosition = Positions[NextPositionIndex];
+
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position,NextPosition.position,speed * Time.deltaTime);
+        }
     }
 
 }
