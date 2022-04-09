74b19f538695b48c6757ddf1b835def1c09e3939
TheHaad <fmuntaz@gmail.com>
2022-04-09 15:20:24 -0400

    add weapon prefab to power up scriptable object

--- Assets/Scripts/PowerUps/Power_Up.cs
+++ Assets/Scripts/PowerUps/Power_Up.cs
@@ -15,6 +15,7 @@ public class Power_Up : ScriptableObject
     public int multiplier = 0;
     public int damage = 0;
     public Sprite sprite;
+    public GameObject weapon_prefab;
     public string power_up_name;
 
 }
