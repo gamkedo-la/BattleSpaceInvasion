a524c5b97fc581df14f8d1e610b2df42c3932deb
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-06-08 20:50:06 +0800

    power up for drone now moving forward

--- Assets/Scripts/PowerUps/Power_Drone1.cs
+++ Assets/Scripts/PowerUps/Power_Drone1.cs
@@ -6,11 +6,17 @@ public class Power_Drone1 : MonoBehaviour
 {
     public GameObject drone1;
     public GameObject drone2;
+    private float speed = 5f;
 
     void Start()
     {
         drone1.SetActive(false);
         drone2.SetActive(false);
+    }
+
+    void Update()
+    {
+        PowerUpDroneMove();
     }
 
    void OnTriggerEnter2D(Collider2D collission) 
@@ -33,6 +39,11 @@ public class Power_Drone1 : MonoBehaviour
 
         }
         
+    }
+
+    private void PowerUpDroneMove()
+    {
+        transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
     }
    
 }
