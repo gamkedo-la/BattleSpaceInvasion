94dee7feeee980e983e5519581edf3348df2ebf5
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-04 09:27:11 +0800

    add animator to UFO1 in Jungle Planet

--- Assets/Scripts/Enemies/MetallicBird.cs
+++ Assets/Scripts/Enemies/MetallicBird.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MetallicBird : MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    //[SerializeField] GameObject player;
 
     // Start is called before the first frame update
     void Start()
