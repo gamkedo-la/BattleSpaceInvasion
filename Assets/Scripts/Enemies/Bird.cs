21ba9de41798529ff07aa5dcd6178fcbaacbd53c
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-05 02:07:18 +0800

    make botTentacles spawn and move

--- Assets/Scripts/Enemies/Bird.cs
+++ Assets/Scripts/Enemies/Bird.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public Transform target;
+   
+    //public Transform target;
     private AudioSource audioSource;
     private Rigidbody2D rb;
     public float t;
@@ -19,18 +20,19 @@ public class Bird : MonoBehaviour
   
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        //rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = target.position - transform.position;
-        //Debug.Log(direction);
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
+        
+        //Vector3 direction = target.position - transform.position;
+        ////Debug.Log(direction);
+        ////float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        ////rb.rotation = angle;
+        //direction.Normalize();
+        //movement = direction;
         Rotate();
     }
 
@@ -45,21 +47,30 @@ public class Bird : MonoBehaviour
 
     void FixedUpdate()
     {
-        MoveBird(movement);
+        //MoveBird(movement);
+        Move();
     }
 
-    void MoveBird(Vector2 direction)
+
+    private void Move()
     {
-       // transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
-        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
-        
-        
-        //Bird will shoot up
-        //Remove the bird head and transition to bird launch (either front or back of jet)
-        //Pick player position at that time and quickly fly toward it.
-        // remove bird launch and turns to bird attack png.
-        // add rotation applied to aim at the point picked.
-        //Bird should only pick that one set of coordinates and fly toward it. Shouldn't continually lock on and follow the player.
-
+        transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
     }
+
+
+
+    //void MoveBird(Vector2 direction)
+    //{
+    //   // transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
+    //    rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+        
+        
+    //    //Bird will shoot up
+    //    //Remove the bird head and transition to bird launch (either front or back of jet)
+    //    //Pick player position at that time and quickly fly toward it.
+    //    // remove bird launch and turns to bird attack png.
+    //    // add rotation applied to aim at the point picked.
+    //    //Bird should only pick that one set of coordinates and fly toward it. Shouldn't continually lock on and follow the player.
+
+    //}
 }
--- Assets/Scripts/UI/SpawnManager.cs
+++ Assets/Scripts/UI/SpawnManager.cs
@@ -53,14 +53,17 @@ public class SpawnManager : MonoBehaviour
             Vector3 redEnemyLeft = new Vector3(Random.Range(2.6f, -7f), 4, 0);
             Instantiate(redSpaceShipLeftPrefab, redEnemyLeft, Quaternion.identity);
 
-            Vector3 asteroid1ToSpawn = new Vector3(12, Random.Range(3f, -3f), 0);
+            Vector3 asteroid1ToSpawn = new Vector3(12, Random.Range(2.6f, -3f), 0);
             Instantiate(asteroid1Prefab, asteroid1ToSpawn, Quaternion.identity);
 
-            Vector3 asteroid2ToSpawn = new Vector3(12, Random.Range(4f, -4f), 0);
+            Vector3 asteroid2ToSpawn = new Vector3(12, Random.Range(2.6f, -4f), 0);
             Instantiate(asteroid1Prefab, asteroid1ToSpawn, Quaternion.identity);
 
-            Vector3 asteroid3ToSpawn = new Vector3(12, Random.Range(4f, -4f), 0);
+            Vector3 asteroid3ToSpawn = new Vector3(12, Random.Range(2.6f, -4f), 0);
             Instantiate(asteroid1Prefab, asteroid3ToSpawn, Quaternion.identity);
+
+            Vector3 botTentaclesToSpawn = new Vector3(12, Random.Range(2.6f, -4f), 0);
+            Instantiate(botTentaclesPrefab, botTentaclesToSpawn, Quaternion.identity);
 
             Vector3 rotationVector = new Vector3(0, 0, 90);
 
@@ -69,7 +72,7 @@ public class SpawnManager : MonoBehaviour
             GameObject newRedEnemy = Instantiate(redSpaceShipPrefab, postToSpawn, Quaternion.Euler(rotationVector));
             newRedEnemy.transform.parent = redEnemyContainer.transform;
 
-            GameObject newRedEnemyLeft = Instantiate(redSpaceShipLeftPrefab, postToSpawn, Quaternion.Euler(rotationVector));
+            GameObject newRedEnemyLeft = Instantiate(redSpaceShipLeftPrefab, redEnemyLeft, Quaternion.Euler(rotationVector));
             newRedEnemyLeft.transform.parent = redEnemyContainer.transform;
 
             GameObject newAsteriod1 = Instantiate(asteroid1Prefab, asteroid1ToSpawn, Quaternion.identity);
@@ -81,6 +84,9 @@ public class SpawnManager : MonoBehaviour
 
             GameObject newAsteriod3 = Instantiate(asteroid3Prefab, asteroid3ToSpawn, Quaternion.identity);
             newAsteriod2.transform.parent = redEnemyContainer.transform;
+
+            GameObject newBotTentacles = Instantiate(botTentaclesPrefab, botTentaclesToSpawn, Quaternion.identity);
+            newBotTentacles.transform.parent = redEnemyContainer.transform;
 
             yield return new WaitForSeconds(3.0f);
 
