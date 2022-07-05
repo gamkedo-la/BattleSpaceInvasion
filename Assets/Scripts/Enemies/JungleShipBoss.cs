a28b95668943d43db11a41258adcda64040b16a1
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-05 17:31:28 +0800

    add some narration in caveScene Entrance

--- Assets/Scripts/Enemies/JungleShipBoss.cs
+++ Assets/Scripts/Enemies/JungleShipBoss.cs
@@ -150,7 +150,7 @@ public class JungleShipBoss : MonoBehaviour
         {
             Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
-            SceneManager.LoadScene("CaveLevel");
+            SceneManager.LoadScene("CaveSceneEntrance");
         }
     }
 }
