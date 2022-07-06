20841285dbb67a08b3cd681bd2dbcd163f206e08
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-06 22:50:36 +0800

    made metallic bird move forward - WIP

--- Assets/Scripts/Enemies/MetallicBird.cs
+++ Assets/Scripts/Enemies/MetallicBird.cs
@@ -4,17 +4,73 @@ using UnityEngine;
 
 public class MetallicBird : MonoBehaviour
 {
-    //[SerializeField] GameObject player;
+    public GameObject explosion;
+    //public Transform target;
+    private AudioSource audioSource;
+    private Rigidbody2D rb;
+    public float t;
+    [SerializeField]
+    private float speed;
+   
+      
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Move();
+
     }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "laser")
+        {
+
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            ScoreManager.instance.AddPoints(1);
+            Destroy(other);
+            Destroy(gameObject);
+        }
+        else if (other.tag == "Player")
+        {
+           
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+          
+
+        }
+        if (other.tag == "largeLaser")
+        {
+
+
+
+          
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            ScoreManager.instance.AddPoints(3);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+
+
+
+        }
+
+    }
+
+
 }
