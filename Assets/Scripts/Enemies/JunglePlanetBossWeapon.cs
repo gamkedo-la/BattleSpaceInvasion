337d512d39aafd03a0974385344d63dc8935f582
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-09 06:53:17 +0800

    Planet Guardian weapon bouncing.

--- Assets/Scripts/Enemies/JunglePlanetBossWeapon.cs
+++ Assets/Scripts/Enemies/JunglePlanetBossWeapon.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class JunglePlanetBossWeapon : MonoBehaviour
 {
     public GameObject shotPrefab;
-
+    private static Transform topMarker;
+    private static Transform bottomMarker;
     private float verticalSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        if (topMarker == null)
+        {
+            GameObject topEdgeGO = GameObject.Find("ScreenTop");
+            topMarker = topEdgeGO.transform;
+
+            GameObject bottomEdgeGO = GameObject.Find("ScreenBottom");
+            bottomMarker = bottomEdgeGO.transform;
+        }
+
+
         verticalSpeed = Random.Range(-1.5f, 1.5f);
         StartCoroutine(FireWithDelay());
 
@@ -18,6 +29,15 @@ public class JunglePlanetBossWeapon : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3.right * -2.0f + Vector3.up * verticalSpeed) * Time.deltaTime;
+        if(transform.position.y > topMarker.position.y && verticalSpeed > 0)
+        {
+            verticalSpeed = -verticalSpeed;
+        }
+
+        if (transform.position.y < bottomMarker.position.y && verticalSpeed < 0)
+        {
+            verticalSpeed = -verticalSpeed;
+        }
     }
    
     IEnumerator FireWithDelay()
