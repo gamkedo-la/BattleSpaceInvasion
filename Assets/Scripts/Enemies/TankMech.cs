18d48833d8c6f805d9bad0c066cbbcf05e6234a2
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-14 19:25:23 +0800

    update tankMechmissile spawning to 2f

--- Assets/Scripts/Enemies/TankMech.cs
+++ Assets/Scripts/Enemies/TankMech.cs
@@ -32,7 +32,7 @@ public class TankMech : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.8f);
+            yield return new WaitForSeconds(2f);
             GameObject shotGO = GameObject.Instantiate(tankMechLaserPrefab) as GameObject;
             shotGO.transform.position = laserSpawnPoint.transform.position;
         }
