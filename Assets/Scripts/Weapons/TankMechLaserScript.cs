6fd9e8e13844b2a0b5f1d955e6af12a542c58041
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-15 21:39:41 +0800

    Missile firing upward tank mech

--- Assets/Scripts/Weapons/TankMechLaserScript.cs
+++ Assets/Scripts/Weapons/TankMechLaserScript.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class TankMechLaserScript : MonoBehaviour
 {
+
+    public GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.up * Time.deltaTime * 4.0f;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "laser")
+        {
+
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            ScoreManager.instance.AddPoints(1);
+            Destroy(other);
+            Destroy(gameObject);
+        }
+        else if (other.tag == "Player")
+        {
+
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null) // perform a null check error handling. 
+            {
+                player.PlayerDamage();
+            }
+
+        }
+        if (other.tag == "largeLaser")
+        {
+
+
+
+
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            ScoreManager.instance.AddPoints(3);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+
+
+
+        }
+
     }
 }
