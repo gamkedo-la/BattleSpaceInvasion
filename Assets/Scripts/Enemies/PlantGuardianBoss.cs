604bee4fc7b1fbdc88c3a556c9f1e29c2468f19c
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-16 08:41:03 +0800

    Adjust planetGuardian weapon spawning and health

--- Assets/Scripts/Enemies/PlantGuardianBoss.cs
+++ Assets/Scripts/Enemies/PlantGuardianBoss.cs
@@ -58,7 +58,7 @@ public class PlantGuardianBoss : MonoBehaviour
         while (true)
         {
 
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(5.0f);
             GameObject newShot = GameObject.Instantiate(weaponPrefab, transform.position, Quaternion.identity);
         }
     }
